517eeb12d79894dab48ba1606c2b0b98dfc066f6
Ivan Baev <ivanbbaev@gmail.com>
2016-06-02 14:22:08 +0300

    da
    daw

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -11,7 +11,7 @@ namespace TestConsole
         static void Main(string[] args)
         {
             Console.WriteLine("cl");
-
+Console.WriteLine("cl23");
             Console.WriteLine("cl2");
         }
     }
